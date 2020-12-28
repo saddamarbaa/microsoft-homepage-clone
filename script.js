2ba99347794c3c822ce7de092f53b2a1371f4bd3
saddamarbaa <saddamarbaas@gmail.com>
2020-12-28 08:15:04 +0700

    Final Build for the Project (jS file)

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document
   .querySelector(".menu-btn")
-  .addEventListener("click", () =>
+  .addEventListener("click", () =,>
     document.querySelector(".main-menu").classList.toggle("show")
   );
